a2ad6841f8c950c71a39e5a2809d1c0f7c2deb8e
Manson Jones <manson.jones@gmail.com>
2020-07-13 20:55:28 +0000

    code cleanup on unit tests

--- src/unit_tests.cpp
+++ src/unit_tests.cpp
@@ -11,31 +11,31 @@ using std::endl;
 void UnitTests::testSystem() {
   System system;
   cout << "System Tests" << std::endl;
-  cout << " MemoryUtilization " << system.MemoryUtilization() << std::endl;
-  cout << " UpTime() " << system.UpTime() << std::endl;
-  cout << " TotalProcesses " << system.TotalProcesses() << std::endl;
-  cout << " RunningProcesses " << system.RunningProcesses() << std::endl;
-  cout << " Kernel() " << system.Kernel() << std::endl;
-  cout << " OperatingSystem() " << system.OperatingSystem() << std::endl;
-  cout << " process ids" << std::endl;
+  cout << " MemoryUtilization " << system.MemoryUtilization() << endl;
+  cout << " UpTime() " << system.UpTime() << endl;
+  cout << " TotalProcesses " << system.TotalProcesses() << endl;
+  cout << " RunningProcesses " << system.RunningProcesses() << endl;
+  cout << " Kernel() " << system.Kernel() << endl;
+  cout << " OperatingSystem() " << system.OperatingSystem() << endl;
+  cout << " process ids" <<endl;
   std::vector<Process> processes = system.Processes();
-  std::cout << " cpu utilization" << system.Cpu().Utilization() << std::endl;
+  cout << " cpu utilization" << system.Cpu().Utilization() << endl;
 }
 
 void UnitTests::testProcess(int pid) {
   Process process(pid);
-  std::cout << "Process Tests" << std::endl;
-  std::cout << "Pid() " << process.Pid() << std::endl;
-  std::cout << "Ram() " << process.Ram() << std::endl;
-  std::cout << "Command() " << process.Command() << std::endl;
-  std::cout << "User() " << process.User() << std::endl;
-  std::cout << "UpTime() " << process.UpTime() << std::endl;
+  cout << "Process Tests" << endl;
+  cout << "Pid() " << process.Pid() << endl;
+  cout << "Ram() " << process.Ram() << endl;
+  cout << "Command() " << process.Command() << endl;
+  cout << "User() " << process.User() << endl;
+  cout << "UpTime() " << process.UpTime() << endl;
 }
 
 void UnitTests::testJiffies(int pid) {
-  std::cout << "LinuxParser::Jiffies() " << LinuxParser::Jiffies() << std::endl;
-  std::cout << "LinuxParser::ActiveJiffies(pid) " << LinuxParser::ActiveJiffies(pid) << std::endl;
-  std::cout << "LinuxParser::ActiveJiffies() " << LinuxParser::ActiveJiffies() << std::endl;
-  std::cout << "LinuxParser::IdleJiffies() " << LinuxParser::IdleJiffies() << std::endl;
+  cout << "LinuxParser::Jiffies() " << LinuxParser::Jiffies() << endl;
+  cout << "LinuxParser::ActiveJiffies(pid) " << LinuxParser::ActiveJiffies(pid) << endl;
+  cout << "LinuxParser::ActiveJiffies() " << LinuxParser::ActiveJiffies() << endl;
+  cout << "LinuxParser::IdleJiffies() " << LinuxParser::IdleJiffies() << endl;
 
 }
