53f7524c25295fb2d3c48a7cb3d7d4b0e73e39be
Manson Jones <manson.jones@gmail.com>
2020-07-13 19:43:15 +0000

    add template function for computing cpu utilization

--- include/CPUMeasurement.h
+++ include/CPUMeasurement.h
@@ -1,6 +1,8 @@
 #ifndef CPUMEASUREMENT_INCLUDE_H
 #define CPUMEASUREMENT_INCLUDE_H
 
+#include <thread>
+#include <chrono>
 
 class CPUMeasurement {
 public:
@@ -16,5 +18,33 @@ protected:
   long cached_active_jiffies_;
   long cached_idle_jiffies_;
 };
+
+
+// Note. Eventually should make this code tighter using
+// Concepts.
+// This template function will only work only classes
+// that are derived from CPUMeasurement.
+
+template <class Type> float CalculateUtilization(Type cpu) {
+  float utilization{0};
+  long activeJiffies = cpu.ActiveJiffies();
+  long idleJiffies = cpu.IdleJiffies();
+  long deltaActiveJiffies = activeJiffies - cpu.getCachedActiveJiffies();
+  long deltaIdleJiffies = idleJiffies - cpu.getCachedIdleJiffies();
+  long delta = deltaActiveJiffies + deltaIdleJiffies;
+
+  if (delta < 1) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    activeJiffies = cpu.ActvieJiffies();
+    idleJiffies = cpu.IdleJiffies();
+    deltaActiveJiffies = activeJiffies - cpu.getCachedActiveJiffies();
+    deltaIdleJiffies = idleJiffies - cpu.getCachedIdleJiffies();
+    delta = deltaActiveJiffies + deltaIdleJiffies;
+  }
+  utilization = static_cast<float>(deltaActiveJiffies/delta);
+  cpu.setCahcedActiveJiffies(activeJiffies);
+  cpu.setCachedIdleJiffies(idleJiffies);
+  return utilization;
+}
 
 #endif
